9d47dbb27d092bbddf4f5a0a8a5ad3cfb83641a4
chuanhao01 <chuanhaolim@gmail.com>
2020-06-20 14:07:37 +0800

    updated with pc tmux script

--- scripts/tmux_startup.sh
+++ scripts/tmux_startup.sh
@@ -6,20 +6,11 @@ tmux start-server
 # Creating the first new session
 tmux new-session -d -s watching -n htop \; \
     new-window -d -n redshift \; \
-	new-window -d -n demo \; \
 	send-keys -t htop 'htop' Enter \; \
-    send-keys -t redshift 'redshift -t 3500:3500' C-m\; \
-	send-keys -t demo 'cd ~/home/Github/Crazy_Ideas/python_web' Enter \; \
+    send-keys -t redshift 'redshift -t 3200:3200' C-m\; \
 
-tmux new-session -d -s own -n BED \; \
-	split-window -v \; \
-	selectp -t 1 \; \
-	send-keys 'cd ~/home/Github/Full_Stack_Web_App' C-m\; \
-	selectp -t 0 \; \
-	send-keys 'cd ~/home/Github/Full_Stack_Web_App' C-m\; \
-	new-window -d -n CS \; \
-	selectw -t CS \; \
-	send-keys 'cd ~/home/Github/CS_CPP' C-m\; \
+tmux new-session -d -s own \; \
+	send-keys 'cd ~/github' C-m\; \
 
 tmux new-session -d -s school -n main \; \
 	send-keys 'goschgit' C-m\; \
