d607755fb2e4c313151633341e68bab1912f09d7
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-10-19 23:35:51 +0800

    making tmux script only have github session

--- scripts/tmux_startup.sh
+++ scripts/tmux_startup.sh
@@ -7,10 +7,7 @@ tmux start-server
 tmux new-session -d -s watching -n htop \; \
     new-window -d -n redshift \; \
 	send-keys -t htop 'htop' Enter \; \
-    send-keys -t redshift 'redshift -t 3200:3200' C-m\; \
+    send-keys -t redshift 'redshift -t 3500:3500' C-m\; \
 
 tmux new-session -d -s own \; \
 	send-keys 'cd ~/github' C-m\; \
-
-tmux new-session -d -s school -n main \; \
-	send-keys 'goschgit' C-m\; \
