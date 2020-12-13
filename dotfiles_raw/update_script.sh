e1a26f4275b97a7b5efd1d7483b4e9804ec0e6e4
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-12-13 15:33:15 +0800

    adding update script for the dotfile_raw

---
+++ dotfiles_raw/update_script.sh
@@ -0,0 +1,14 @@
+# Zsh
+cp ~/.zshrc .
+cp ~/.p10k.zsh .
+
+# tmux
+cp ~/.tmux.conf .
+cp ~/.tmux.conf.local .
+
+# Config file
+# I3
+cp -r ~/.config/i3 ./.config/
+
+# redshift
+cp ~/.config/redshift.conf ./.config/
