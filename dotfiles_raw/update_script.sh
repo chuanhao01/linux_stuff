daff6ba53d341b24440a61d4009f6a942d94e340
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2021-01-15 20:41:16 +0800

    adding condarc config

--- dotfiles_raw/update_script.sh
+++ dotfiles_raw/update_script.sh
@@ -6,9 +6,11 @@ cp ~/.p10k.zsh .
 cp ~/.tmux.conf .
 cp ~/.tmux.conf.local .
 
-# Config file
 # I3
 cp -r ~/.config/i3 ./.config/
 
 # redshift
 cp ~/.config/redshift.conf ./.config/
+
+# condarc
+cp ~/.condarc .
